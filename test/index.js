c9d66e9bc90398908a93901a296ddaa4a10a9c03
Willem Olding <willemolding@gmail.com>
2019-05-31 12:13:19 +1000

    add multi-agent test

--- test/index.js
+++ test/index.js
@@ -5,15 +5,32 @@ Scenario.setTape(require("tape"))
 
 const dnaPath = "./dist/game-matchmaking.dna.json"
 const agentAlice = Config.agent("alice")
+const agentBob = Config.agent("bob")
 const dna = Config.dna(dnaPath)
 const instanceAlice = Config.instance(agentAlice, dna)
-const scenario = new Scenario([instanceAlice])
+const instanceBob = Config.instance(agentBob, dna)
+const scenario = new Scenario([instanceAlice, instanceBob])
 
-scenario.runTape("description of example test", async (t, { alice }) => {
+scenario.runTape("Alice can create a proposal and retrieve it", async (t, { alice }) => {
   // Make a call to a Zome function
   // indicating the function, and passing it an input
   const addr = await alice.callSync("main", "create_proposal", {"message" : "sup"})
   console.log(addr)
-  // check for equality of the actual and expected results
   t.deepEqual(addr.Ok.length, 46)
+
+  // if a function takes no arguments we must still pass an empty object
+  const proposals = await alice.callSync("main", "get_proposals", {})
+  console.log(proposals)
+  t.deepEqual(proposals.Ok.length, 1)
+})
+
+// It is possible to write scenarios with multiple agents!
+scenario.runTape("Bob can see the proposal created by Alice", async (t, { alice, bob }) => {
+  const addr = await alice.callSync("main", "create_proposal", {"message" : "sup"})
+  console.log(addr)
+  t.deepEqual(addr.Ok.length, 46)
+
+  const proposals = await bob.callSync("main", "get_proposals", {})
+  console.log(proposals)
+  t.deepEqual(proposals.Ok.length, 1)
 })
