71fbda9ab470801edd0007ecc1a0fc9b6a1ddb01
Willem Olding <willemolding@gmail.com>
2019-06-24 10:36:44 +1000

    exercise complete

--- test/index.js
+++ test/index.js
@@ -25,23 +25,23 @@ const diorama = new Diorama({
 diorama.registerScenario("Alice can create a proposal and retrieve it", async (s, t, { alice }) => {
   // Make a call to a Zome function
   // indicating the function, and passing it an input
-  const addr = await alice.call("main", "create_proposal", {message : "sup"})
+  const addr = await alice.callSync("main", "create_proposal", {message : "sup"})
   console.log(addr)
   t.deepEqual(addr.Ok.length, 46, 'proposal was added successfully')
 
   // if a function takes no arguments we must still pass an empty object
-  const proposals = await alice.call("main", "get_proposals", {})
+  const proposals = await alice.callSync("main", "get_proposals", {})
   console.log(proposals)
   t.deepEqual(proposals.Ok.length, 1, 'alice can see own proposal')
 })
 
 // It is possible to write scenarios with multiple agents!
 diorama.registerScenario("Bob can see the proposal created by Alice", async (s, t, { alice, bob }) => {
-  const addr = await alice.call("main", "create_proposal", {message : "sup"})
+  const addr = await alice.callSync("main", "create_proposal", {message : "sup"})
   console.log(addr)
   t.deepEqual(addr.Ok.length, 46, 'proposal was added successfully')
 
-  const proposals = await bob.call("main", "get_proposals", {})
+  const proposals = await bob.callSync("main", "get_proposals", {})
   console.log(proposals)
   t.deepEqual(proposals.Ok.length, 1, 'bob can see proposal')
 })
